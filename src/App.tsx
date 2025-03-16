e497f4b7fbaa5c2e1e104f6cb71a7ccd12c129a3
Arkadiusz Kałkus <kalkus@10g.pl>
2025-03-16 01:16:40 +0100

    Add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Box, Container } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Container maxW="100%" p={0}>
+      <Box p={4} bg="blue.400" minH="100vh">
+        
+      </Box>
+    </Container>
   )
 }
 
